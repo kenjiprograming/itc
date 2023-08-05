089961ee0c6f6274600620bcf9be3826856811cb
iamkenjigido <iamkenji.programing@gmail.com>
2023-08-05 16:14:22 +0900

    :sparkles: add 404 logic 3-6

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import NippoModel
 from .forms import NippoFormsClass
 
@@ -13,7 +13,7 @@ def nippoListView(request):
 def nippoDetailView(request, pk):
     template_name = 'nippo/nippo-detail.html'
     ctx = {}
-    obj = NippoModel.objects.get(pk=pk)
+    obj = get_object_or_404(NippoModel, pk=pk)
     ctx['obj'] = obj
 
     return render(request, template_name, ctx)
@@ -33,7 +33,7 @@ def nippoCreateView(request):
 
 def nippoUpdateView(request, pk):
     template_name = 'nippo/nippo-form.html'
-    obj = NippoModel.objects.get(pk=pk)
+    obj = get_object_or_404(NippoModel, pk=pk)
     initial_value = {'title': obj.title, 'content': obj.content}
 
     form = NippoFormsClass(request.POST or initial_value)
