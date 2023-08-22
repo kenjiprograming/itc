4210fbba5c811370072567d483ec60764c425410
iamkenjigido <iamkenji.programing@gmail.com>
2023-08-22 20:53:29 +0900

    :lipstick: [4-6] init form class

--- src/nippo/forms.py
+++ src/nippo/forms.py
@@ -5,6 +5,12 @@ class NippoFormsClass(forms.Form):
     title = forms.CharField(label='タイトル', widget=forms.TextInput(attrs={'placeholder': 'タイトル...'}))
     content = forms.CharField(label='内容', widget=forms.Textarea(attrs={'placeholder': '内容...'}))
 
+    def __init__(self, *args, **kwards):
+        for field in self.base_fields.values():
+            field.widget.attrs.update({'class':'form-control'})
+        super().__init__(*args, ** kwards)
+
+
 class ImageUploadForm(forms.ModelForm):
     class Meta:
         model = ImageUpload
